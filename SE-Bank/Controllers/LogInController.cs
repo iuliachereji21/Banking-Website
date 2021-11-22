0fe6ba3c36c4c4fc43b0378529031254752d7852
Iulia Chereji <iuliachereji@gmail.com>
2021-11-22 15:38:29 +0200

    log in youtube

---
+++ SE-Bank/Controllers/LogInController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SE_Bank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SE_Bank.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if(userModel.UserName == "ana" && userModel.Password == "0000")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}
